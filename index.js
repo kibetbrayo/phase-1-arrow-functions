5350d44b50b1703d358ac77e1991c90eaacb5f33
iam <brian.kibet2@student.moringaschool.com>
2023-06-14 03:17:42 +0000

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+// has a function called divide
+const divide = function(a, b){
+    return 20;
+};
+// arrow function called square
+const square = (num) =>{
+    return num * num;
+}
+//arrow function called add
+const add =(num1,num2)=>{
+    return num1 + num2 ;
+}
+
+
+
+
+
+    
